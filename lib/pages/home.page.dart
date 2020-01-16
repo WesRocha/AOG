8f12ea7b88a5e8f216314a263e1bf99462c6b2ff
WesRocha <39282367+WesRocha@users.noreply.github.com>
2020-01-16 14:49:15 -0300

    my third commit

--- lib/pages/home.page.dart
+++ lib/pages/home.page.dart
@@ -10,7 +10,7 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  Color _color = Colors.deepPurple;
+  Color _color = Colors.orange;
   var _gasCtrl = new MoneyMaskedTextController();
   var _alcCtrl = new MoneyMaskedTextController();
   var _busy = false;
